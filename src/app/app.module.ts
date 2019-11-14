3d1a7f04b8b4044fa20bba4a8dc77cf306d4cba8
safronman <safronmanbox@gmail.com>
2019-11-14 16:06:33 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule
